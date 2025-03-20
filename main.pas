404471e6012d6d8a1825ee3ccfa4bff12766208f
Fernando Artur Luithardt <fluithardt@gmail.com>
2025-03-20 20:50:14 -0300

    básico da Fila

--- main.pas
+++ main.pas
@@ -1,106 +1,102 @@
 program fila_simples;
 uses crt;
 
-const tamanho_fila_socios       = 500;
-const tamanho_fila_visitante    = 300;
-const tamanho_fila_normal       = 2200;
+const 
+  tamanho_fila_socios    = 500;
+  tamanho_fila_visitante = 300;
+  tamanho_fila_normal    = 2200;
 
 type
-   vet = array[1..tamanho_fila_socios] of integer;
+  vet = array[1..tamanho_fila_socios] of integer;
 
-var fila: vet;
-    opcao,ultima_pos_fila_socio, ultima_pos_fila_visitante, ultima_pos_fila_normal:integer;
+var 
+  fila: vet;
+  opcao, ultima_pos_fila_socio, ultima_pos_fila_visitante, ultima_pos_fila_normal: integer;
 
-procedure inicializa(var pos:integer);
+procedure inicializa(var pos: integer);
 begin
-   pos:=0;
+  pos := 0;
 end;
 
-
-procedure ler_elemento(var el:integer);
+procedure ler_elemento(var el: integer);
 begin
   writeln('Digite o elemento');
   readln(el);
 end;
 
-function cheia(posicao:integer, tamanho:integer):boolean;
-
+function cheia(posicao, tamanho: integer): boolean;
 begin   
-  cheia := true;
-  if posicao < tamanho then
-     cheia:=false;
+  cheia := posicao >= tamanho;
 end;
 
-function vazia(posicao:integer):boolean;
-
+function vazia(posicao: integer): boolean;
 begin   
-  vazia:=false;
-  if posicao = 0 then
-     vazia := true;
+  vazia := posicao = 0;
 end;
 
-procedure insere_fila(var f:vet; var posicao, elemento:integer, tamanho:integer);
+procedure insere_fila(var f: vet; var posicao: integer; elemento: integer; tamanho: integer);
 begin
-  if not (cheia(posicao, tamanho)) then 
-	begin
-    posicao := posicao+1;
-    f[posicao] : =elemento; 
+  if not cheia(posicao, tamanho) then
+  begin
+    posicao := posicao + 1;
+    f[posicao] := elemento; 
   end
-  else begin
-      writeln('Fila cheia');
-      readkey
-  end
+  else 
+  begin
+    writeln('Fila cheia');
+    readkey;
+  end;
 end;
 
-procedure remove_fila(var f:vet;var posicao,el:integer);
-var i:integer;
+procedure remove_fila(var f: vet; var posicao, el: integer);
+var 
+  i: integer;
 begin
   if vazia(posicao) then 
-	begin
-     writeln('Fila vazia');
-     readkey
+  begin
+    writeln('Fila vazia');
+    readkey;
   end
-  else begin
-     el:=f[1];
-     writeln ('Elemento Removido',el);
-     readkey;
-        for i := 1 to posicao - 1 do
-            f[i]:=f[i + 1];
-     posicao := posicao - 1
-  end
-end;
-
-procedure consulta_fila(f:vet;posi:integer;var el:integer;cor1,cor2:integer);
-begin
-  textcolor(cor1);
-  if not (vazia(posi)) then
-	begin
-     el:=f[posi];
-     writeln ('Primeiro elemento ',el);
-	end
   else 
-      writeln('Fila vazia');
-  readkey;
-	textcolor(cor2) 
+  begin
+    el := f[1];
+    writeln('Elemento Removido ', el);
+    readkey;
+    for i := 1 to posicao - 1 do
+      f[i] := f[i + 1];
+    posicao := posicao - 1;
+  end;
 end;
 
-procedure escreve_fila(f:vet;posi:integer;texto:string;cor1,cor2:integer);
-var i:integer;
-
+procedure consulta_fila(f: vet; posi: integer; var el: integer; cor1, cor2: integer);
 begin
   textcolor(cor1);
+  if not vazia(posi) then
+  begin
+    el := f[posi];
+    writeln('Primeiro elemento ', el);
+  end
+  else 
+    writeln('Fila vazia');
+    readkey;
+    textcolor(cor2);
+end;
+
+procedure escreve_fila(f: vet; posi: integer; texto: string);
+var 
+  i: integer;
+begin
   writeln(texto);
   writeln;
-  if not (vazia(posi)) then
-    for I:=1 to posi do
-      write (f[i],' ')
+  if not vazia(posi) then
+    for i := 1 to posi do
+      write(f[i], ' ')
   else 
-      writeln('Fila vazia');
+    writeln('Fila vazia');
   readkey;
-	textcolor(cor2)     
 end;
 
-procedure menu (var op_menu:integer);
+procedure menu(var op_menu: integer);
 begin
         clrscr;
         textcolor(blue);
@@ -121,28 +117,35 @@ begin
         textcolor(blue);
 end;
 
-{Programa Principal}
-
+{ Programa Principal }
 begin
-    clrscr;
-    inicializa(ultima_pos_fila_socio);
-    opcao:=0;
-    while opcao<> 5 do begin
+  clrscr;
+  inicializa(ultima_pos_fila_socio);
+  inicializa(ultima_pos_fila_visitante);
+  inicializa(ultima_pos_fila_normal);
+  
+  opcao := 0;
+  while opcao <> 5 do 
+  begin
     menu(opcao);
-        if opcao = 1
-        then 
-            begin
-                insere_fila(fila, ultima_pos_fila_socio, 1, tamanho_fila_socios);
-            end
-        else if opcao = 2 
-        then 
-            begin
-                insere_fila(fila, tamanho_fila_visitante, 1, tamanho_fila_socios);
-            end
+    
+    if opcao = 1 then 
+      insere_fila(fila, ultima_pos_fila_socio, 1, tamanho_fila_socios)
+    else if opcao = 2 then 
+      insere_fila(fila, ultima_pos_fila_visitante, 1, tamanho_fila_visitante)
+    else if opcao = 3 then 
         else if opcao = 3 then 
-            begin
-                insere_fila(fila, tamanho_fila_normal, 1, tamanho_fila_socios);
-            end
-        
-   end;
+    else if opcao = 3 then 
+        else if opcao = 3 then 
+    else if opcao = 3 then 
+        else if opcao = 3 then 
+    else if opcao = 3 then 
+        else if opcao = 3 then 
+    else if opcao = 3 then 
+        else if opcao = 3 then 
+    else if opcao = 3 then 
+        else if opcao = 3 then 
+    else if opcao = 3 then 
+      insere_fila(fila, ultima_pos_fila_normal, 1, tamanho_fila_normal);
+  end;
 end.
