a6bb5d60e5d2888af17bc1be40ca4620974d2baa
Fernando Artur Luithardt <fluithardt@gmail.com>
2025-03-20 21:21:59 -0300

    Adicionando Pulha

--- main.pas
+++ main.pas
@@ -7,13 +7,23 @@ const
     tamanho_fila_normal    = 2200;
 
 type
-    vet = array[1..tamanho_fila_socios] of integer;
+    vet = array[1..5000] of integer;
 
 var 
     fila_socios: vet;
     fila_visitante: vet;
     fila_normal: vet;
     opcao, ultima_pos_fila_socio, ultima_pos_fila_visitante, ultima_pos_fila_normal: integer;
+
+var
+    topo_pilha_arquibancada: integer;
+    topo_pilha_geral: integer;
+    topo_pilha_visitante: integer;
+
+    pilha_arquibancada: vet;
+    pilha_geral: vet;
+    pilha_visitante: vet;
+
 
 procedure inicializaFila(var pos: integer);
 begin
@@ -31,9 +41,9 @@ begin
     cheia := posicao >= tamanho;
 end;
 
-function vazia(posicao: integer): boolean;
+function FilaVazia(posicao: integer): boolean;
 begin   
-    vazia := posicao = 0;
+    FilaVazia := posicao = 0;
 end;
 
 procedure insere_fila(var f: vet; var posicao: integer; elemento: integer; tamanho: integer);
@@ -55,7 +65,7 @@ var
   i: integer;
 
 begin
-  if vazia(posicao) then 
+  if FilaVazia(posicao) then 
   begin
     writeln('Fila vazia');
     readkey;
@@ -74,7 +84,7 @@ end;
 procedure consulta_fila(f: vet; posi: integer; var el: integer; cor1, cor2: integer);
 begin
   textcolor(cor1);
-  if not vazia(posi) then
+  if not FilaVazia(posi) then
   begin
     el := f[posi];
     writeln('Primeiro elemento ', el);
@@ -91,7 +101,7 @@ var
 begin
   writeln(texto);
   writeln;
-  if not vazia(posi) then
+  if not FilaVazia(posi) then
     for i := 1 to posi do
       write(f[i], ' ')
   else 
@@ -100,47 +110,67 @@ begin
 end;
 
 
+function PilhaVazia(var topo:integer): Boolean;
+begin
+  PilhaVazia := (topo = 0);
+end;
 
+// Remove um elemento da pilha.
+procedure remove_pilha(var topo:integer);
+var
+    i: Integer;
+begin
+    if PilhaVazia(topo) then
+        WriteLn('Ingressos Esgotados.')
+    else
+    begin
+        topo := topo - 1;
+    end;
+end;
 
 
 procedure menu(var op_menu: integer);
 begin
-        clrscr;
-        textcolor(blue);
-        writeln ('    MENU    ');
-        writeln ('------------');
-        writeln;
-        writeln ('1 - Adicionar na fila de Sócios');
-        writeln ('2 - Adicionar na fila de Visitantes');
-        writeln ('3 - Adicionar na fila de Normal');
+    clrscr;
+    textcolor(blue);
+    writeln ('    MENU    ');
+    writeln ('------------');
+    writeln;
+    writeln ('1 - Adicionar na fila de Sócios');
+    writeln ('2 - Adicionar na fila de Visitantes');
+    writeln ('3 - Adicionar na fila de Normal');
 
 
-        writeln;
-        write('    ==> ');
-        textcolor(red);
-        readln (op_menu);
+    writeln;
+    write('    ==> ');
+    textcolor(red);
+    readln (op_menu);
 
-        clrscr;
-        textcolor(blue);
+    clrscr;
+    textcolor(blue);
 end;
 
 { Programa Principal }
 begin
-  clrscr;
-  inicializaFila(ultima_pos_fila_socio);
-  inicializaFila(ultima_pos_fila_visitante);
-  inicializaFila(ultima_pos_fila_normal);
-  opcao := 0;
+    clrscr;
+    inicializaFila(ultima_pos_fila_socio);
+    inicializaFila(ultima_pos_fila_visitante);
+    inicializaFila(ultima_pos_fila_normal);
 
-  while opcao <> 5 do 
-  begin
-    menu(opcao);
-    if opcao = 1 then 
-      insere_fila(fila_socios, ultima_pos_fila_socio, 1, tamanho_fila_socios)
-    else if opcao = 2 then 
-      insere_fila(fila_visitante, ultima_pos_fila_visitante, 1, tamanho_fila_visitante)
-    else if opcao = 3 then 
-      insere_fila(fila_normal, ultima_pos_fila_normal, 1, tamanho_fila_normal);
-  end; 
+    topo_pilha_arquibancada := 2000;
+    topo_pilha_geral        := 700;
+    topo_pilha_visitante    := 300;
+
+    opcao := 0;
+    while opcao <> 5 do 
+    begin
+        menu(opcao);
+        if opcao = 1 then 
+            insere_fila(fila_socios, ultima_pos_fila_socio, 1, tamanho_fila_socios)
+        else if opcao = 2 then 
+            insere_fila(fila_visitante, ultima_pos_fila_visitante, 1, tamanho_fila_visitante)
+        else if opcao = 3 then 
+            insere_fila(fila_normal, ultima_pos_fila_normal, 1, tamanho_fila_normal);
+    end; 
 
 end.
